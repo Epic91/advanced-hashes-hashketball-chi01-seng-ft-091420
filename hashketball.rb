a91055f193919ac0047cd3697bfef910ecc0e3cf
Evelyn Hernandez <hernandeze791@gmail.com>
2020-09-08 03:10:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -175,17 +175,15 @@ def player_numbers(input)
   return output
 end
 
-def player_stats name
-  player_hash = {}
-  game_hash.each do |team_position, team_data|
-   return team_data[:players].each do |player_name, player_data|
-     if player_name == name
-       return player_data
-       elsif player_name =! name
-       return nil
-     end
- end
-end
+def player_stats(name) #takes argument of player name and returns hash of players stats
+  hash = game_hash
+  hash.each do |teams_playing, data|
+    data[:players].each do |player_name, player_data|
+      if player_name == name
+        return hash[teams_playing][:players][name]
+      end 
+    end 
+  end 
 end
 
 def big_shoe_rebounds
